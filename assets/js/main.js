b9e0cc05d3b72f3c5efd900e9706cd4c34c9196c
Nadia-Kadri <nadiagrace96@gmail.com>
2024-06-03 12:51:40 -0400

    Create send email functionality to contact buttons, change resume button to <a> tag with resume link, edit skills section styling, add projects card

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+const contactButtons = document.querySelectorAll(".contact");
+
+contactButtons.forEach(button => button.addEventListener("click", () => window.location.href = "mailto:nadiagrace96@gmail.com"));
